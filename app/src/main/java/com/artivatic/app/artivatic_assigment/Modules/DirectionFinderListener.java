f07b736c6cac9d0d3b6df49768254fe19f8ea92f
SUNIL <puttursunil@gmail.com>
2018-01-17 19:46:56 +0530

    chnaged

--- app/src/main/java/com/artivatic/app/artivatic_assigment/Modules/DirectionFinderListener.java
+++ app/src/main/java/com/artivatic/app/artivatic_assigment/Modules/DirectionFinderListener.java
@@ -7,9 +7,7 @@ import retrofit2.Call;
 import retrofit2.http.POST;
 import retrofit2.http.Query;
 
-/**
- * Created by Mai Thanh Hiep on 4/3/2016.
- */
+
 public interface DirectionFinderListener {
     void onDirectionFinderStart();
     void onDirectionFinderSuccess(List<Route> route);
